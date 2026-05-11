003704fb8b8c7fcae8cd81ea53dd0824c5c0df4e
cauaosp <cauaosp20@gmail.com>
2026-05-11 14:25:27 -0300

    feat: jornal cearaagora foi adicionado

--- fetch_opovo.py
+++ fetch_opovo.py
@@ -206,6 +206,52 @@ def fetch_verdemares(url, headers):
     return articles
 
 
+def fetch_cearaagora(url, params, headers):
+    articles = []
+
+    try:
+        response = requests.get(
+            url,
+            params=params,
+            headers=headers,
+        )
+
+        print(response.status_code, "cearaagora")
+
+        items = response.json()
+
+        for item in items:
+            author_id = item["author"]
+            categorias_id = item.get("categories", [])
+            categorias = []
+
+            url_autor = f"https://cearaagora.com.br/wp-json/wp/v2/users/{author_id}"
+            autor = requests.get(url_autor, headers=headers).json()
+
+            for i in range(len(categorias_id)):
+                url_categoria = f"https://cearaagora.com.br/wp-json/wp/v2/categories/{categorias_id[i]}"
+                categoria = requests.get(url_categoria, headers=headers).json()
+                categorias.append(categoria["name"])
+
+            articles.append(
+                {
+                    "titulo": item["title"]["rendered"],
+                    "subtitulo": item["excerpt"]["rendered"],
+                    "categoria": "",
+                    "autor": autor["name"],
+                    "dataPublicacao": item["date"],
+                    "link": item["link"],
+                    "jornal": "cearaagora",
+                    "created_at": creation_time(),
+                }
+            )
+
+    except requests.exceptions.RequestException as e:
+        print(f"Erro de conexão: {e}")
+
+    return articles
+
+
 if __name__ == "__main__":
     jornais = {
         "opovo": {
@@ -241,12 +287,26 @@ if __name__ == "__main__":
             "url": "https://g1.globo.com/rss/g1/ce/ceara",
             "headers": {"User-Agent": "Mozilla/5.0"},
         },
+        "cearaagora": {
+            "url": "https://cearaagora.com.br/wp-json/wp/v2/posts",
+            "params": {"page": "1", "per_page": "30"},
+            "headers": {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+            },
+        },
     }
 
     n = 0
 
     while n == 0:
-        data = {"opovo": [], "gcmais": [], "dn": [], "oestadoce": [], "globoce": []}
+        data = {
+            "opovo": [],
+            "gcmais": [],
+            "dn": [],
+            "oestadoce": [],
+            "globoce": [],
+            "cearaagora": [],
+        }
 
         data["opovo"].extend(
             fetch_opovo(
@@ -272,8 +332,14 @@ if __name__ == "__main__":
         data["globoce"].extend(
             fetch_verdemares(jornais["globoce"]["url"], jornais["globoce"]["headers"])
         )
-
         data["dn"].extend(fetch_dn(jornais["dn"]["url"], jornais["dn"]["headers"]))
+        data["cearaagora"].extend(
+            fetch_cearaagora(
+                jornais["cearaagora"]["url"],
+                jornais["cearaagora"]["params"],
+                jornais["cearaagora"]["headers"],
+            )
+        )
 
         with open("data/artigos_ceara.json", "w", encoding="utf-8") as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
--- teste.py
+++ teste.py
@@ -6,58 +6,67 @@ from utils.functions import creation_time
 
 
 def fetch_cearaagora(url, params, headers):
-    print("começo")
-
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
-    }
+    articles = []
+    print(url, params, headers)
 
     try:
         response = requests.get(
-            "",
+            url,
+            params=params,
             headers=headers,
-            timeout=10,  # Evita ficar travado
         )
 
-        if response.status_code == 200:
-            post_json = response.json()
-            author_id = post_json["author"]
-            print(f"Autor: {author_id}")
-            categorias = post_json.get("categories", [])
-            print(f"Categorias: {categorias}")
+        print(response.status_code, "cearaagora")
+
+        items = response.json()
+
+        for item in items:
+            author_id = item["author"]
+            categorias_id = item.get("categories", [])
+            categorias = []
 
             url_autor = f"https://cearaagora.com.br/wp-json/wp/v2/users/{author_id}"
             autor = requests.get(url_autor, headers=headers).json()
-            print(f"Nome do autor: {autor['name']}")
 
-            for i in range(len(categorias)):
-                url_categoria = f"https://cearaagora.com.br/wp-json/wp/v2/categories/{categorias[i]}"
+            for i in range(len(categorias_id)):
+                url_categoria = f"https://cearaagora.com.br/wp-json/wp/v2/categories/{categorias_id[i]}"
                 categoria = requests.get(url_categoria, headers=headers).json()
-                print(f"Categoria {i + 1}: {categoria['name']}")
+                categorias.append(categoria["name"])
 
-        else:
-            print(f"Erro {response.status_code}: Falha na requisição")
+            articles.append(
+                {
+                    "titulo": item["title"]["rendered"],
+                    "subtitulo": item["excerpt"]["rendered"],
+                    "categoria": "",
+                    "autor": autor["name"],
+                    "dataPublicacao": item["date"],
+                    "link": item["link"],
+                    "jornal": "cearaagora",
+                    "created_at": creation_time(),
+                }
+            )
 
     except requests.exceptions.RequestException as e:
         print(f"Erro de conexão: {e}")
+
+    return articles
 
 
 if __name__ == "__main__":
     jornal = {
         "cearaagora": {
-            "url": "https://cearaagora.com.br/wp-json/wp/v2/posts/371190",
+            "url": "https://cearaagora.com.br/wp-json/wp/v2/posts",
             "params": {"page": "1", "per_page": "30"},
-            "headers": {"User-Agent": "Mozilla/5.0"},
+            "headers": {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+            },
         }
     }
 
-    fetch_cearaagora(
+    articles_list = fetch_cearaagora(
         jornal["cearaagora"]["url"],
         jornal["cearaagora"]["params"],
         jornal["cearaagora"]["headers"],
     )
 
-    # print(f"artigo: {json.dumps(dn_articles_list[0], indent=4)}")
-    # print(f"total: {len(dn_articles_list)} artigos")
-
-    # print(json.dumps(dn_articles_list, indent=4))
+    print(f"total: {len(articles_list)} artigos")
